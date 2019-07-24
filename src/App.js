dd5b46a33adb92e80b17bf2cb5bf407d8d446bae
Dave Parfrey <dparfrey@gmail.com>
2019-07-24 13:23:56 -0400

    24

--- src/App.js
+++ src/App.js
@@ -33,8 +33,17 @@ class App extends Component {
     .then((res) => this.setState({ note: res.data, showNote: true }) )
   }
 
-  submitNote = (data) => {
-    axios.post(urlFor('notes'), data)
+  performSubmissionRequest = (data, id) => {
+    console.log(id);
+    if (id) {
+      return axios.patch(urlFor(`notes/${id}`), data);
+    } else {
+      return axios.post(urlFor('notes'), data);
+    }
+  }
+
+  submitNote = (data, id) => {
+    this.performSubmissionRequest(data, id)
       .then((res) => this.setState({ showNote: false }) )
       .catch((err) => console.log(err.response.data) );
   }
