fb548726515256f505b5762adadeead48644c5e9
Dave Parfrey <dparfrey@gmail.com>
2019-07-24 11:50:15 -0400

    16

---
+++ src/components/NoteCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class NoteCard extends React.Component {
+
+  render() {
+    return (
+      <div className="note-card-container">
+        NoteCard Component
+      </div>
+    );
+  }
+}
+
+export default NoteCard;
+
